c003380c95dfb9db8a98a091ec1ee6ef6b807056
rmccrear <113715288+rmccrear@users.noreply.github.com>
2023-05-06 03:25:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict'
+// Don't change lines 6 or 7. 
+// Lines 6 and 7 get the current data,
+// and they store the current year as a number
+// in a variable called currentYear.
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+
+// TODO: Ask the user what year they were born in and save the value in a variable.
+
+
+// TODO: Calculate how old the user is by using the value that is already stored in the variable called currentYear.
+
+
+// TODO: Use alert to tell the user's current age.
+
